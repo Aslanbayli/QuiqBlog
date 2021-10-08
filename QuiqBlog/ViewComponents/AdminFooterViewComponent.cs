08bc520fd0c83d156cbf2d5214b29cfa76968464
Aslanbayli <aliaslanbayli@gmail.com>
2021-10-08 00:50:30 -0400

    admin views

---
+++ QuiqBlog/ViewComponents/AdminFooterViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiqBlog.ViewComponents
+{
+    public class AdminFooterViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Factory.StartNew(() => { return View(); });
+        }
+    }
+}
+
